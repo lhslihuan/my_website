410a238293878de7994dda9289b86a944402a670
lhslihuan <316683328@qq.com>
2015-01-29 23:14:55 +0800

    换风格为蓝色

--- guide/guide.js
+++ guide/guide.js
@@ -2,9 +2,11 @@
  * Created by LSH on 2015/1/25.
  */
 seajs.use(['./rotate-show'], function (rotateShow) {
-    var $innerCircle = $('.j_inner_circle');
+    $(function () {
+        var $innerCircle = $('.j_inner_circle');
 
-    rotateShow.rotateShow(6, function () {
-        $innerCircle.fadeIn();
-    });
+        rotateShow.rotateShow(6, function () {
+            $innerCircle.fadeIn();
+        });
+    })
 });
